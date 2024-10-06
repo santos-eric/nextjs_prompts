c27aec86319cdba621e40352823bc8cc5f5896c8
santos-eric <esantosdev77@gmail.com>
2024-10-06 11:29:47 -0600

    fixed styling issue where content was centered

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,26 @@
-import '@styles/globals.css'
+import "@styles/globals.css";
+
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-    title: 'nextjsPrompts',
-    description: 'Discover and Share AI Prompts'
-}
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
 
-const RootLayout = ({children}) => {
-  return (
-      <html lang='en'>
-          <body>
-              <div className="main">
-                  <div className="gradient"></div>
-                  <main className="app">
-                      {children}
-                  </main>
-            </div>
-          </body>
-    </html>
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
 
-  )
-}
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
+    </body>
+  </html>
+);
 
-export default RootLayout
+export default RootLayout;
