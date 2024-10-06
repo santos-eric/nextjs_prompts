a1fea342b46b618fb292f255498b3a06a6c38457
santos-eric <esantosdev77@gmail.com>
2024-10-06 12:36:20 -0600

    added logic for logging in with null state, created useEffect to get and set provider data using null useState, added logic for displaying data if user is logged in

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,22 @@ import {useState, useEffect} from 'react'
 import {signIn, signOut, useSession, getProviders} from'next-auth/react'
 
 const Nav = () => {
+  const isUserLoggedIn = true
+
+  const [providers, setProviders] = useState(null)
+
+  // create setProviders var which waits for the response var, which awaits getProviders function provided by next-auth, then setProviders state based on the response within the useEffect and then actually set the state for providers
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders()
+
+      setProviders(response)
+    }
+
+    setProviders()
+  }, [])
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href='/' className="flex gap-2 flex-center">
@@ -16,7 +32,50 @@ const Nav = () => {
           className='object-contain'
           alt='prompt logo'
         />
+        <p className="logo_text">nextjsPrompts</p>
       </Link>
+
+      {/* mobile navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className='flex gap-3 md:gap-5'>
+            <Link href='create-prompt'
+              className="black_btn">
+              Create Post
+            </Link>
+
+            <button type='button' onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+
+            <Link href='/profile'>
+              <Image
+                src='/assets/images/logo.svg'
+                width={37}
+                height={37}
+                className='rounded-full'
+                alt='profile'
+              />
+            </Link>
+          </div>) : (
+          <>
+
+              {/* will list login providers such as Google */}
+              {providers &&
+            object.values(providers).map((provider) => (
+              <button
+                type='button'
+                key={provider.name}
+                onClick={() => signIn(provider.id)}
+                className='black_btn'
+              >
+
+              </button>
+            ))
+          }
+          </>
+        )}
+      </div>
     </nav>
   )
 }
