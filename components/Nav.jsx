d2bac275904cd64b233a5a7b6c9fc56d6debfd0b
santos-eric <esantosdev77@gmail.com>
2024-10-07 06:37:47 -0600

    added conditional rendering based on state of user logged in, state is set as null and the useEffect keeps checking for log in status, dropdown icon not working due to this error, will fix state issue soon

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,42 +1,40 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
-import Image from 'next/image'
-import {useState, useEffect} from 'react'
-import {signIn, signOut, useSession, getProviders} from'next-auth/react'
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
   const isUserLoggedIn = true
 
   const [providers, setProviders] = useState(null)
+  const [toggleDropdown, setToggleDropdown] = useState(false)
 
   // create setProviders var which waits for the response var, which awaits getProviders function provided by next-auth, then setProviders state based on the response within the useEffect and then actually set the state for providers
 
   useEffect(() => {
-    const setProviders = async () => {
-      const response = await getProviders()
-
-      setProviders(response)
-    }
-
-    setProviders()
-  }, [])
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
+  }, []);
 
   return (
-    <nav className="flex-between w-full mb-16 pt-3">
-      <Link href='/' className="flex gap-2 flex-center">
+    <nav className='flex-between w-full mb-16 pt-3'>
+      <Link href='/' className='flex gap-2 flex-center'>
         <Image
           src='/assets/images/logo.svg'
+          alt='logo'
           width={30}
           height={30}
           className='object-contain'
-          alt='prompt logo'
         />
-        <p className="logo_text">nextjsPrompts</p>
+        <p className='logo_text'>Promptopia</p>
       </Link>
 
       {/* mobile navigation */}
-      <div className="sm:flex hidden">
+      <div className="sm:hidden flex relative">
         {isUserLoggedIn ? (
           <div className='flex gap-3 md:gap-5'>
             <Link href='create-prompt'
@@ -55,8 +53,37 @@ const Nav = () => {
                 height={37}
                 className='rounded-full'
                 alt='profile'
+                // not recommended to change state calling the previous state, create a second callback function within onClick instead
+                onClick={() => setToggleDropdown((prev) => !prev)}
               />
             </Link>
+            {toggleDropdown && (
+              <div className="dropdown">
+                <Link
+                  href='profile'
+                  className="dropdown_link"
+                  conCLick={() => setToggleDropdown(false)}
+                >
+                My Profile
+                </Link>
+                <Link
+                  href='create-prompt'
+                  className="dropdown_link"
+                  onCLick={() => setToggleDropdown(false)}
+                >
+                Create Prompt
+                </Link>
+                <button
+                  type='button'
+                  onCLick={() => {
+                    setToggleDropdown(false);
+                    signOut()
+                  }}
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
           </div>) : (
           <>
 
